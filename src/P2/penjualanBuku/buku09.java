15828242118e4553754450faa2b7a7eb79ecf6a2
Duta Kurnia A <dutaka2945@gmail.com>
2024-02-29 07:56:33 +0700

    P2_percobaan 1_1C_09

---
+++ src/P2/penjualanBuku/buku09.java
@@ -0,0 +1,28 @@
+package P2.penjualanBuku;
+
+public class buku09 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Stok: " + stok);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+
+    
+}
