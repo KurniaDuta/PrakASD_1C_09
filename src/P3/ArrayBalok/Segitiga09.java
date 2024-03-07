308cdfdb21f0686c7687d3f37f9961d1f9c4cfbe
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 11:35:15 +0700

    P3_percobaan 3_pertanyaan 3_1C_09

--- src/P3/ArrayBalok/Segitiga09.java
+++ src/P3/ArrayBalok/Segitiga09.java
@@ -1,5 +1,7 @@
 package P3.ArrayBalok;
 
+import java.lang.Math;
+
 public class Segitiga09 {
     public int alas;
     public int tinggi;
@@ -7,5 +9,13 @@ public class Segitiga09 {
     public Segitiga09(int a, int t) {
         alas = a;
         tinggi = t;
+    }
+
+    public int hitungLuas() {
+        return alas * tinggi / 2;
+    }
+
+    public double hitungKeliling() {
+        return alas + tinggi + Math.sqrt(alas * alas + tinggi * tinggi);
     }
 }
