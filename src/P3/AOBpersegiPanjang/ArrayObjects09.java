814a19c84443fcb045b495eb58a1bdcfbf58e8b7
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 09:40:29 +0700

    P3_percobaan 1_pertanyaan 2 _1C_09

--- src/P3/AOBpersegiPanjang/ArrayObjects09.java
+++ src/P3/AOBpersegiPanjang/ArrayObjects09.java
@@ -6,20 +6,20 @@ public class ArrayObjects09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        PersegiPanjang09[] ppArray = new PersegiPanjang09[3];
+        PersegiPanjang09[][] ppArray = new PersegiPanjang09[3][3];
 
-        for (int i = 0; i < ppArray.length; i++) {
-            ppArray[i] = new PersegiPanjang09();
+        for (int i = 0; i < ppArray[0].length; i++) {
+            ppArray[i][i] = new PersegiPanjang09();
             System.out.println("Persegi panjang ke-" + i);
             System.out.print("Masukkan panjang: ");
-            ppArray[i].panjang = input.nextInt();
+            ppArray[i][i].panjang = input.nextInt();
             System.out.print("Masukkan lebar: ");
-            ppArray[i].lebar = input.nextInt();
+            ppArray[i][i].lebar = input.nextInt();
         }
 
-        for (int i = 0; i < ppArray.length; i++) {
+        for (int i = 0; i < ppArray[0].length; i++) {
             System.out.println("Persegi Panjang ke-" + i);
-            System.out.println("Panjang: " + ppArray[i].panjang + ", Lebar: " + ppArray[i].lebar);
+            System.out.println("Panjang: " + ppArray[i][i].panjang + ", Lebar: " + ppArray[i][i].lebar);
         }
     }
 }
