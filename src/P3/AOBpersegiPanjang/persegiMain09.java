9e18e2f0549a64b9894aa4f4a1ea16585bb2f45d
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 09:52:42 +0700

    P3_percobaan 1_pertanyaan 3 _1C_09 perbaikan

--- src/P3/AOBpersegiPanjang/persegiMain09.java
+++ src/P3/AOBpersegiPanjang/persegiMain09.java
@@ -3,6 +3,9 @@ package P3.AOBpersegiPanjang;
 public class persegiMain09 {
     public static void main(String[] args) {
         persegi09[] pgArray = new persegi09[100];
+
+        pgArray[5] = new persegi09();
         pgArray[5].sisi = 20;
+        System.out.println("sisi: " + pgArray[5].sisi);
     }
 }
