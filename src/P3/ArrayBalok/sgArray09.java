2e7d3e1168cf879b0c90f795d70cecee09aa3fe4
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 11:38:40 +0700

    P3_percobaan 3_pertanyaan 4_1C_09

---
+++ src/P3/ArrayBalok/sgArray09.java
@@ -0,0 +1,12 @@
+package P3.ArrayBalok;
+
+public class sgArray09 {
+    public static void main(String[] args) {
+        Segitiga09[] sgArray = new Segitiga09[100];
+
+        sgArray[0] = new Segitiga09(10, 4);
+        sgArray[1] = new Segitiga09(20, 10);
+        sgArray[2] = new Segitiga09(15, 6);
+        sgArray[3] = new Segitiga09(25, 10);
+    }
+}
