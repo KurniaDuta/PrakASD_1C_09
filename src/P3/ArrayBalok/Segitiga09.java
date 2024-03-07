40fa5df3f51fc5df04faf8c825aa657a5eae4ec7
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 11:25:35 +0700

    P3_percobaan 3_pertanyaan 2_1C_09

---
+++ src/P3/ArrayBalok/Segitiga09.java
@@ -0,0 +1,11 @@
+package P3.ArrayBalok;
+
+public class Segitiga09 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga09(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+}
