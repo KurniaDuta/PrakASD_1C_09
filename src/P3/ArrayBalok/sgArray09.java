f0fb4da7c7a110f59069fca18f46e1d2f5acce38
Duta Kurnia A <dutaka2945@gmail.com>
2024-03-07 11:49:39 +0700

    P3_percobaan 3_pertanyaan 5_1C_09

--- src/P3/ArrayBalok/sgArray09.java
+++ src/P3/ArrayBalok/sgArray09.java
@@ -2,11 +2,16 @@ package P3.ArrayBalok;
 
 public class sgArray09 {
     public static void main(String[] args) {
-        Segitiga09[] sgArray = new Segitiga09[100];
+        Segitiga09[] sgArray = new Segitiga09[4];
 
         sgArray[0] = new Segitiga09(10, 4);
         sgArray[1] = new Segitiga09(20, 10);
         sgArray[2] = new Segitiga09(15, 6);
         sgArray[3] = new Segitiga09(25, 10);
+
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("Luas segitiga ke-" + i + ": " + sgArray[i].hitungLuas());
+            System.out.printf("Keliling segitiga ke-%d: %.2f\n", i, sgArray[i].hitungKeliling());
+        }
     }
 }
