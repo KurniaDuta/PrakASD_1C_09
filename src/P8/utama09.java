3da43da8a45a88a5120d9aebbea5a87fb9513b19
KurniaDuta <dutaka2945@gmail.com>
2024-04-18 09:09:24 +0700

    P8_Percobaan 1_pertanyaan 4_1C_09

--- src/P8/utama09.java
+++ src/P8/utama09.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class utama09 {
     public static void main(String[] args) {
-        gudang09 gudang = new gudang09(7);
         Scanner input = new Scanner(System.in);
+        System.out.println("Program Gudang");
+        System.out.print("Masukkan kapasitas gudang: ");
+        int kapasitas = input.nextInt();
+        gudang09 gudang = new gudang09(kapasitas);
 
         while (true) {
-            System.out.println("\nMenu:\n1. Tambah Barang\n2. Ambil Barang\n3. Tampilkan tumpukan barang\n4. Keluar");
+            System.out.println("\nMenu:\n1. Tambah Barang\n2. Ambil Barang\n3. Tampilkan tumpukan barang\n4. Lihat barang teratas\n5. Keluar");
             System.out.print("--> ");
             int pilihan = input.nextInt();
             input.nextLine();
@@ -32,6 +35,9 @@ public class utama09 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
                     System.exit(0);
                     break;
                 default:
