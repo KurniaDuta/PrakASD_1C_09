c8391a114b75b9baeae02ca33e4432c1c28cb018
KurniaDuta <dutaka2945@gmail.com>
2024-05-16 09:55:30 +0700

    P11_Percobaan 2_1C_09

--- src/P11/SingleLinkedList/SLLMain09.java
+++ src/P11/SingleLinkedList/SLLMain09.java
@@ -14,5 +14,17 @@ public class SLLMain09 {
         singLL.print();
         singLL.insertAt(3, 833);
         singLL.print();
+
+        System.out.println("Data pada index k-1: " + singLL.getData(1));
+        System.out.println("Data 3 berada pada index: " + singLL.indexOf(760));
+
+        singLL.remove(999);
+        singLL.print();
+        singLL.removeAt(0);
+        singLL.print();
+        singLL.removeFirst();
+        singLL.print();
+        singLL.removeLast();
+        singLL.print();
     }
 }
