11b29fa3f068aea088f5da3ea1ff7ed3349c1b19
KurniaDuta <dutaka2945@gmail.com>
2024-06-12 21:01:05 +0700

    P15_Tugas 1_1C_09

--- src/P15/graphMain09.java
+++ src/P15/graphMain09.java
@@ -5,22 +5,60 @@ import java.util.Scanner;
 public class graphMain09 {
     public static void main(String[] args) throws Exception {
         Scanner input09 = new Scanner(System.in);
-        graph09 gedung = new graph09(6);
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        gedung.degree(0);
-        gedung.printGraph();
-        gedung.removeEdge(1, 3);
-        gedung.printGraph();
+        String menu[] = { "Add Edge", "Remove Edge", "Degree", "Print Graph", "Cek Edge", "Exit" };
+        int pilih;
 
-        System.out.print("Masukkan gedung asal: ");
-        int asal = input09.nextInt();
-        System.out.print("Masukkan gedung tujuan: ");
-        int tujuan = input09.nextInt();
-        gedung.hasPath(asal, tujuan);
+        System.out.print("Masukkan Jumlah Vertex: ");
+        int vertex = input09.nextInt();
+        graph09 gedung = new graph09(vertex);
+        input09.nextLine();
+
+        do {
+            for (int i = 0; i < menu.length; i++) {
+                System.out.println((i + 1) + ". " + menu[i]);
+            }
+            System.out.println("--------------------------");
+            System.out.print("Pilih --> ");
+            pilih = input09.nextInt();
+
+            switch (pilih) {
+                case 1:
+                    System.out.println("Masukkan vertex pertama: ");
+                    int v1 = input09.nextInt();
+                    System.out.println("Masukkan vertex kedua: ");
+                    int v2 = input09.nextInt();
+                    System.out.println("Masukkan Jarak: ");
+                    int jarak = input09.nextInt();
+                    gedung.addEdge(v1, v2, jarak);
+                    break;
+                case 2:
+                    System.out.println("Masukkan vertex pertama: ");
+                    int v11 = input09.nextInt();
+                    System.out.println("Masukkan vertex kedua: ");
+                    int v22 = input09.nextInt();
+                    gedung.removeEdge(v11, v22);
+                    break;
+                case 3:
+                    gedung.degree(0);
+                    break;
+                case 4:
+                    gedung.printGraph();
+                    break;
+                case 5:
+                    System.out.println("Masukkan vertex pertama: ");
+                    int v111 = input09.nextInt();
+                    System.out.println("Masukkan vertex kedua: ");
+                    int v222 = input09.nextInt();
+                    gedung.hasPath(v111, v222);
+                    break;
+                case 6:
+                    System.out.println("Terima Kasih");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        } while (pilih != 0 && pilih >= 5);
     }
 }
